d79928252a75a4144898925784e9136ca95fd501
cvele0 <vladancvjetkovic@gmail.com>
2022-10-26 17:34:37 +0200

    Modified gitignore

--- src/main/java/LocalStorageImplementation.java
+++ src/main/java/LocalStorageImplementation.java
@@ -2,99 +2,108 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.List;
 
-public class LocalStorageImplementation implements StorageSpecification {
-
+public class LocalStorageImplementation extends StorageSpecification {
   public static void main(String[] args) {
 
   }
 
   @Override
-  public void createRootFolder() {
+  void createRootFolder(Configuration configuration) {
 
   }
 
   @Override
-  public void createRootFolder(Configuration configuration) {
+  void setConfigurationSizeOfStorage(int i) {
 
   }
 
   @Override
-  public void createFolderOnSpecifiedPath(String s) {
+  void setConfigurationExtensions(List<String> list) {
 
   }
 
   @Override
-  public void putFileOnSpecifiedPath(List<File> list, String s) {
+  void setConfigurationNumberOfFiles(int i) {
 
   }
 
   @Override
-  public void deleteFileOrDirectory(String s) {
+  void createFolderOnSpecifiedPath(String s) {
 
   }
 
   @Override
-  public void deleteFileOrDirectory(String s, String s1) {
+  void putFileOnSpecifiedPath(List<File> list, String s) {
 
   }
 
   @Override
-  public void moveFileFromDirectoryToAnother(String s, String s1, String s2) {
+  void deleteFileOrDirectory(String s) {
 
   }
 
   @Override
-  public void downloadFileOrDirectory(String s, String s1) {
+  void deleteFileOrDirectory(String s, String s1) {
 
   }
 
   @Override
-  public void renameFileOrDirectory(String s, String s1, String s2) {
+  void moveFileFromDirectoryToAnother(String s, String s1, String s2) {
 
   }
 
   @Override
-  public HashMap<String, FileMetadata> filesFromDirectory(String s) {
+  void downloadFileOrDirectory(String s, String s1) {
+
+  }
+
+  @Override
+  void renameFileOrDirectory(String s, String s1, String s2) {
+
+  }
+
+  @Override
+  HashMap<String, FileMetadata> filesFromDirectory(String s) {
     return null;
   }
 
   @Override
-  public HashMap<String, FileMetadata> filesFromChildrenDirectory(String s) {
+  HashMap<String, FileMetadata> filesFromChildrenDirectory(String s) {
     return null;
   }
 
   @Override
-  public HashMap<String, FileMetadata> allFilesFromDirectoryAndSubdirectory(String s) {
+  HashMap<String, FileMetadata> allFilesFromDirectoryAndSubdirectory(String s) {
     return null;
   }
 
   @Override
-  public HashMap<String, String> filesFromDirectoryExt(String s, List<String> list) {
+  HashMap<String, String> filesFromDirectoryExt(String s, List<String> list) {
     return null;
   }
 
   @Override
-  public HashMap<String, String> filesFromChildrenDirectoryExt(String s, List<String> list) {
+  HashMap<String, String> filesFromChildrenDirectoryExt(String s, List<String> list) {
     return null;
   }
 
   @Override
-  public HashMap<String, String> allFilesFromDirectoryAndSubdirectoryExt(String s, List<String> list) {
+  HashMap<String, String> allFilesFromDirectoryAndSubdirectoryExt(String s, List<String> list) {
     return null;
   }
 
   @Override
-  public HashMap<String, String> filesFromDirectorySubstring(String s, String s1) {
+  HashMap<String, String> filesFromDirectorySubstring(String s, String s1) {
     return null;
   }
 
   @Override
-  public String folderNameByFileName(String s) {
+  String folderNameByFileName(String s) {
     return null;
   }
 
   @Override
-  public List<String> returnFilesInDateInterval(String s, Date date, Date date1) {
+  List<String> returnFilesInDateInterval(String s, Date date, Date date1) {
     return null;
   }
 }
